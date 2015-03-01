4b7c4e3528688600f747db49515fe817953e2ae5
lxgzmy <lxgzmy@gmail.com>
2015-03-01 21:08:49 +1100

    project set up

---
+++ MessageQueue/MessageQueue/MessageQueue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageQueue
+{
+    public partial class MessageQueue : ServiceBase
+    {
+        public MessageQueue()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
